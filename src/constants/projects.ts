4519f29cc8c07558fd104d2a3ea734a5c44c7bf4
Jason Au <danryan808512@gmail.com>
2025-02-12 02:03:00 -0800

    Removed unnecessary components and localized some info from template

---
+++ src/constants/projects.ts
@@ -0,0 +1,19 @@
+import svgl from "../../public/svgl.png";
+import stockin from "../../public/stockin.png";
+
+export const PROJECTS = [
+    {
+      title: "Snobify",
+      image: stockin as ImageMetadata,
+      link: "https://github.com/friidryce/Snobify/",
+      preview: "https://stockin-demo.vercel.app",
+      status: "In Development",
+    },
+    {
+        title: "Maintenance Tracker",
+          image: svgl as ImageMetadata,
+          link: "https://github.com/friidryce/maintenance-tracker",
+          preview: "https://github.com/friidryce/maintenance-tracker",
+          status: "In Development",
+    },
+]
