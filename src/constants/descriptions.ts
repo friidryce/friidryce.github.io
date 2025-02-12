666eedb701e84d362eceec8e42122bc7b5d9bd8f
Jason Au <danryan808512@gmail.com>
2025-02-12 03:28:47 -0800

    Changed embeds and added soundcloud

---
+++ src/constants/descriptions.ts
@@ -0,0 +1,9 @@
+export const ABOUT = {
+    name: "Jason Au",
+    summary1: "Hey there! Do you ever try to do something on an app, just to realize you can't? I'm here to fix that. As a recent grad who specialized in HCI, I'm looking full-time dev positions where I can fix frustrations and implement intuitive features. ",
+    summary2: "I'm always looking for a chance to pick up new technologies and it's always a fun challenge to just how fast I can do it.",
+    summary3: "When I'm not smacking my keyboard, I enjoy jumping out of planes, zooming down snowy mountains on two feet, and twirling knobs to move dancefloors. I would die for drum & bass and I've been DJing since 2022, having played at SoCal clubs like Bloom, EQ, Avenue PB, and Circle OC. Here are my latest mixes below!",
+    location: "San Diego, CA",
+    email: "danryan808512@gmail.com",
+    phone: "+1(858)380-9518"
+}
